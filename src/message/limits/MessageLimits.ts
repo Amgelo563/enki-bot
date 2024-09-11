c8f2b0db7f38c001a4d1c8a31299c939716aab7a
Amgelo563 <email@amgelo563.me>
2024-09-11 00:35:53 -0500

    fix: remove extra 0 from MessageLimits.Content

--- src/message/limits/MessageLimits.ts
+++ src/message/limits/MessageLimits.ts
@@ -1,6 +1,6 @@
 // https://discord.com/developers/docs/resources/channel#create-message-jsonform-params
 export const MessageLimits = {
-  Content: 20000,
+  Content: 2000,
   Embeds: 10,
   // Undocumented, but should be 10 iirc (I don't think anyone would ever use more than 10 files either way)
   Files: 10,
